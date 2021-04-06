bcb85c7733c7260fde302609c0e189b05ccbe6d8
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-04-06 17:23:12 +0600

    working with otp sending on email

--- lib/PhoneOTP/screens/register.dart
+++ lib/PhoneOTP/screens/register.dart
@@ -5,8 +5,42 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text("Register"),
-    ));
+      appBar: AppBar(
+        title: Text("Register"),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        color: Colors.blue,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                height: 200,
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: UnderlineInputBorder(), labelText: 'Email'),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'OTP CODE'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
